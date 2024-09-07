ae7f50d3d0b797f61aa901b33c53f702da624b67
Vighneshwara C <61262139+vighc@users.noreply.github.com>
2024-09-07 14:19:06 +0000

    Adding Projects

--- scripts.js
+++ scripts.js
@@ -1,2 +1,21 @@
 // Placeholder for future scripts
+let currentProjectIndex = 0;
+const projects = document.querySelectorAll('.project');
+const totalProjects = projects.length;
+
+function showProject(index) {
+  const container = document.querySelector('.carousel-container');
+  container.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function nextProject() {
+  currentProjectIndex = (currentProjectIndex + 1) % totalProjects;
+  showProject(currentProjectIndex);
+}
+
+function prevProject() {
+  currentProjectIndex = (currentProjectIndex - 1 + totalProjects) % totalProjects;
+  showProject(currentProjectIndex);
+}
+
 console.log("Portfolio website loaded.");
