70b4ca22f768867ce64f0dce42a630ae865529cb
Vighneshwara C <61262139+vighc@users.noreply.github.com>
2024-09-08 08:04:02 +0000

    test

--- my-portfolio/next.config.mjs
+++ my-portfolio/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    output: 'export',
+    distDir: 'out'
+    // Other configurations...
+  };
+  
 export default nextConfig;
+  
