93a1febf7617cf6cfcf969a6642fe842aff5a3e0
Vighneshwara C <61262139+vighc@users.noreply.github.com>
2024-09-08 14:49:31 +0000

    Skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,15 +1,52 @@
-// components/Skills.js
 import React from 'react';
 
-const Skills = () => (
-  <section>
-    <h2>Skills</h2>
-    <ul>
-      <li>Python, SQL, Bash</li>
-      <li>Google Cloud Platform (ACE, PDE & PDBE Certified), AWS</li>
-      <li>Apache Spark, DBT, Airflow, Databricks, Kafka, Git, Terraform, SSIS</li>
-    </ul>
-  </section>
-);
+const skillsData = [
+  { category: 'Language', name: 'Python', rating: '★★★★☆' },
+  { category: 'Language', name: 'SQL', rating: '★★★★☆' },
+  { category: 'Language', name: 'Bash', rating: '★★★☆☆' },
+  { category: 'Cloud', name: 'Google Cloud Platform(GCP)', rating: '★★★★☆' },
+  { category: 'Cloud', name: 'Amazon Web Services(AWS)', rating: '★★★★☆' },
+  { category: 'Tools/Frameworks', name: 'Apache Spark', rating: '★★★★☆' },
+  { category: 'Tools/Frameworks', name: 'DBT', rating: '★★★☆☆' },
+  { category: 'Tools/Frameworks', name: 'Airflow', rating: '★★★★☆' },
+  { category: 'Tools/Frameworks', name: 'Databricks', rating: '★★★★☆' },
+  { category: 'Tools/Frameworks', name: 'Kafka', rating: '★★★☆☆' },
+  { category: 'Tools/Frameworks', name: 'Terraform', rating: '★★★★☆' },
+  { category: 'Tools/Frameworks', name: 'SSIS', rating: '★★★☆☆' },
+];
+
+const Skills = () => {
+  const groupedSkills = skillsData.reduce((acc, skill) => {
+    if (!acc[skill.category]) {
+      acc[skill.category] = [];
+    }
+    acc[skill.category].push(skill);
+    return acc;
+  }, {});
+
+  return (
+    <section>
+      <h2>Skills</h2>
+      {Object.keys(groupedSkills).map((category, index) => (
+        <div key={index} className="skill-category">
+          <h4>{category}:&nbsp;</h4>
+          <div className="skill-list">
+            {groupedSkills[category].map((skill, index) => (
+              <div key={index} className="skill-item">
+                {skill.name}
+                <div className="skill-popup">
+                  <img src={`/Files/skill_logos/${skill.name}.png`} alt={skill.name} />
+                  <br />
+                  {skill.rating}
+                </div>
+                {index < groupedSkills[category].length - 1 && ', '}
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+};
 
 export default Skills;
