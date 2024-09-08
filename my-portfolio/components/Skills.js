165cab05ba2695b0bd1fc01d88c10123d6eca9e0
Vighneshwara C <61262139+vighc@users.noreply.github.com>
2024-09-08 06:26:39 +0000

    test

---
+++ my-portfolio/components/Skills.js
@@ -0,0 +1,15 @@
+// components/Skills.js
+import React from 'react';
+
+const Skills = () => (
+  <section>
+    <h2>Skills</h2>
+    <ul>
+      <li>Python, SQL, Bash</li>
+      <li>Google Cloud Platform (ACE, PDE & PDBE Certified), AWS</li>
+      <li>Apache Spark, DBT, Airflow, Databricks, Kafka, Git, Terraform, SSIS</li>
+    </ul>
+  </section>
+);
+
+export default Skills;
